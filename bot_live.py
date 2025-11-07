d8e1c716862a1c20bc29bc3332ee11469241fac1
Mihaja <arfalisoamihaja@gmail.com>
2025-11-07 13:17:56 +0300

    [ADD] Live chats messages

--- bot_live.py
+++ bot_live.py
@@ -1,13 +1,20 @@
 # coding utf-8
 import json
 import requests
-import youtube_oauth
 
 from urllib.parse import urlparse
 from googleapiclient.discovery import build
 
 youtube = None
 comments = []
+
+def is_structurally_valid_url(url_string):
+    try:
+        result = urlparse(url_string)
+        print(result)
+        return all([result.netloc])
+    except ValueError:
+        return False
 
 def set_unlisted_stream(stream=None):
     if not stream:
@@ -25,69 +32,52 @@ def get_participants():
     pass
 
 def get_live_details(video_id):
-    request = youtube.liveBroadcasts().list(
-        part='snippet,contentDetails,status',
+    request = youtube.videos().list(
+        part='snippet,statistics,liveStreamingDetails',
         id=video_id
     )
     response = request.execute()
-    # content = response.get('items', [])
-    # content = content[0] if content else []
+    content = response.get('items', [])[0]
 
-    # res = {
-    #     'id': content.get('id'),
-    #     'title': content['snippet']['title'],
-    #     'channelTitle': content['snippet']['channelTitle'],
-    #     'liveBroadcastContent': content['snippet']['liveBroadcastContent'],
-    #     'commentCount': int(content.get('statistics')['commentCount'])
-    # }
+    res = {
+        'id': content.get('id'),
+        'title': content['snippet']['title'],
+        'channelTitle': content['snippet']['channelTitle'],
+        'liveBroadcastContent': content['snippet']['liveBroadcastContent'],
+        'liveStreamingDetails': content.get('liveStreamingDetails'),
+        'commentCount': int(content.get('statistics')['commentCount'])
+    }
     print("\nHere is some details about the youtube video.")
-    print(json.dumps(content, indent=4))
+    print(json.dumps(res, indent=4))
+    if 'liveBroadcastContent' in res and res['liveBroadcastContent'] != 'live':
+        print("The video is not live currently. Aborting")
+        exit(0)
     return content
 
-def get_live_chats(video_id, keyword):
+def get_live_chats(liveChatId, keyword):
     # TODO: Also check comments for live video
-    next_page = 'first'
-    while next_page:
-        if next_page == 'first':
-            next_page = None
-
-        request = youtube.commentThreads().list(
-            part="id,snippet",
-            videoId=video_id,
-            textFormat='plainText',
-            searchTerms=keyword,
-            order='time',
-            maxResults=100,
-            pageToken=next_page
-        )
-        response = request.execute()
-        content = response.get('items')
-        for ct in content:
-            c = ct.get('snippet').get('topLevelComment')
-            s = c.get('snippet')
-            comments.append({
-                "kind": c['kind'],
-                "id": c['id'],
-                "authorDisplayName": s.get('authorDisplayName'),
-                "textDisplay": s.get('textDisplay'),
-                "publishedAt": s.get('publishedAt'),
-                "updatedAt": s.get('updatedAt'),
-            })
-        
-        next_page = response.get('nextPageToken')
-    
-    print(f"Found {len(comments)} comments with keyword '{keyword}':")
-    return comments
+    request = youtube.liveChatMessages().list(
+        liveChatId=liveChatId,
+        part='snippet,authorDetails',
+        maxResults=200
+    )
+    response = request.execute()
+    print(json.dumps(response, indent=4))
+    return response
 
 
-youtube = youtube_oauth.connect()
+# youtube = youtube_oauth.connect()
+youtube = build(
+    'youtube', 'v3',
+    developerKey="AIzaSyBB9KR0avf3MJ3njd56raMUpjTOor9dqvo",
+)
 # link = set_unlisted_stream()
 # mekOjGBYeoQ    UCI
 # vQQEaSnQ_bs    Python Youtube API
 # TEuRjhhYkvA     Outlier Odoo - Gestion d'un parc
-
-get_live_details("onogE5uepoQ")
-# key = set_keyword()
-# get_video_comments("onogE5uepoQ", keyword=key)
+live = get_live_details("l8PMl7tUDIE")
+key = set_keyword()
+liveChatId = live.get('liveStreamingDetails', {}).get('activeLiveChatId')
+get_live_chats(liveChatId, keyword=key)
 
 
