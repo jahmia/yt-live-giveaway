abcd6cc4b87344c5a0e6b61e53ebc48da766fda9
jahmia <arfalisoamiaja@gmail.com>
2025-11-11 13:37:48 +0300

    [ADD] Connect by api key

--- youtube_oauth.py
+++ youtube_oauth.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 
+from dotenv import load_dotenv
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
@@ -8,41 +9,55 @@ from googleapiclient.discovery import build
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 credentials = None
 
-# token.pickle stores the user's credentials from previously successful logins
-if os.path.exists('token.pickle'):
-    print('Loading Credentials From File...')
-    with open('token.pickle', 'rb') as token:
-        credentials = pickle.load(token)
+
+def get_credentials():
+    # token.pickle stores the user's credentials from previously successful logins
+    if os.path.exists('token.pickle'):
+        print('Loading Credentials From File...')
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
 
 
-'''
-If there are no valid credentials available,
-then either refresh the token or log in.
-'''
-if not credentials or not credentials.valid:
-    if credentials and credentials.expired and credentials.refresh_token:
-        print('Refreshing Access Token...')
-        credentials.refresh(Request())
-    else:
-        print('Fetching New Tokens...')
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'client_secrets.json',
-            scopes=[
-                'https://www.googleapis.com/auth/youtube.readonly',
-                'https://www.googleapis.com/auth/youtube',
-                'https://www.googleapis.com/auth/youtube.force-ssl',
-            ]
-        )
+    '''
+    If there are no valid credentials available,
+    then either refresh the token or log in.
+    '''
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            print('Refreshing Access Token...')
+            credentials.refresh(Request())
+        else:
+            print('Fetching New Tokens...')
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secrets.json',
+                scopes=[
+                    'https://www.googleapis.com/auth/youtube.readonly',
+                    'https://www.googleapis.com/auth/youtube',
+                    'https://www.googleapis.com/auth/youtube.force-ssl',
+                ]
+            )
 
-        flow.run_local_server(port=8080, prompt='consent',
-                              authorization_prompt_message='')
-        credentials = flow.credentials
+            flow.run_local_server(port=8080, prompt='consent',
+                                authorization_prompt_message='')
+            credentials = flow.credentials
 
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as f:
-            print('Saving Credentials for Future Use...')
-            pickle.dump(credentials, f)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as f:
+                print('Saving Credentials for Future Use...')
+                pickle.dump(credentials, f)
 
 def connect():
+    crendentials = get_credentials()
     youtube = build('youtube', 'v3', credentials=credentials)
+    return youtube
+
+def connect_by_api_key():
+    load_dotenv()
+
+    api_key = os.getenv('GOOGLE_API_KEY')
+
+    if not api_key:
+        print("API Key not found in .env file or environment variables.")
+        exit(1)
+    youtube = build('youtube', 'v3', developerKey=api_key)
     return youtube
